2d5ab61d9ada4aaf550cb3399bf88ce9d29f6d18
Sriram Dhavala <sriramdhavala2011@gmail.com>
2026-07-05 12:27:31 +0000

    Add area script

---
+++ area.py
@@ -0,0 +1,5 @@
+base = int(input("Enter the base of the triangle: "))
+height = int(input("Enter the height of the triangle: "))
+area = (base * height) / 2
+print("The area of the triangle is: " + str(area) + " square units")
+
