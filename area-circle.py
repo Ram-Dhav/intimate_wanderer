77d34598459ec9240d57abeb6b1753a314d2866a
Sriram Dhavala <sriramdhavala2011@gmail.com>
2026-07-07 04:39:51 +0000

    Area of a circle

---
+++ area-circle.py
@@ -0,0 +1,5 @@
+name = input("Hello! What is your name? ")
+radius = float(input(f"Hello {name}, enter the radius of the circle: "))
+pi = 3.1415927
+area = (pi * (radius ** 2))
+print(f"The area of the circle is {area} , {name}")
